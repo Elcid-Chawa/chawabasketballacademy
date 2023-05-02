88401daac0e955e4dc218d95e59fb7352d2c3b35
Elcid-Chawa <elcidchawa@gmail.com>
2023-05-02 08:53:50 +0100

    added sponsor links
    Signed-off-by: Elcid-Chawa <elcidchawa@gmail.com>

---
+++ src/components/Sponsors/index.js
@@ -0,0 +1,54 @@
+import { MDBCol, MDBContainer, MDBRow } from "mdb-react-ui-kit";
+
+export default function Sponsors(){
+    return(
+        <section className="fluid p-4 bg-dark bg-gradient">
+        <h3 className="text-white">In Collaboration with:</h3>      
+            <MDBContainer >
+                <MDBRow className="d-flex justify-content-center align-items-center">
+                    
+                    <MDBCol size='2'>
+                    <img
+                        src='/images/Chawex.png'
+                        className="img-thumbnail rounded-pill"
+                        alt='...'
+                        width='50%'
+                    />
+                    </MDBCol>
+                    <MDBCol size='2'>
+                        <img
+                            src='/images/Chawex.png'
+                            className="img-thumbnail rounded-pill"
+                            alt='...'
+                            width='50%'
+                        />
+                    </MDBCol>
+                    <MDBCol size='2'>
+                        <img
+                            src='/images/swrllogo.jpeg'
+                            className="img-thumbnail rounded-pill"
+                            alt='...'
+                            width='50%'
+                        />
+                    </MDBCol>
+                    <MDBCol size='2'>
+                        <img
+                            src='/logo2.png'
+                            className="img-thumbnail"
+                            alt='...'
+                            width='50%' 
+                        />
+                    </MDBCol>
+                    <MDBCol size='2'>
+                        <img
+                            src='/logo2.png'
+                            className="img-thumbnail"
+                            alt='...'
+                            width="50%"
+                        />
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+        </section>
+    );
+}
