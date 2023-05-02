0fccbb1742030b7dbbdf691b6c8d63d7bd973a08
Elcid-Chawa <elcidchawa@gmail.com>
2023-05-02 08:54:37 +0100

    added classes
    Signed-off-by: Elcid-Chawa <elcidchawa@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import BasketballAcademy from './components/BasketballAcademy';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Sponsors from './components/Sponsors';
 
 function App() {
   return (
     <>
       <Header />
       <BasketballAcademy />
+      <Sponsors />
       <Footer />
     </>
     
